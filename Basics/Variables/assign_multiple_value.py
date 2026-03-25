ee5a14656c4a36c1fa20369f91777861d30fd541
BrianOrea00 <deathscytheiv@gmail.com>
2026-03-25 21:48:36 +0800

    Assign Multiple Value

---
+++ Basics/Variables/assign_multiple_value.py
@@ -0,0 +1,23 @@
+# Assign Multiple value
+# in here we can assign a Multiple variable and have Multiple data in one line
+# example1
+x1, y1, z1 = "first data", "second data", "third data"
+print(x1)
+print(y1)
+print(z1)
+
+# in the second example we can assign 1 data in Multiple variable using equal sign(=)
+# example2
+x2 = y2 = z2 = "Same data"
+print(x2)
+print(y2)
+print(z2)
+
+# we have here a interesting one the unpacking
+# this one we can extract the data in a list , tuple etc to give it to Multiple variable
+# example3
+Food = ["Hotdog","Pizza","Burger"]
+x3, y3, z3 = Food
+print(x3)
+print(y3)
+print(z3)
---
+++ Basics/Variables/assign_multiple_valueAct.py
@@ -0,0 +1,24 @@
+# AI generated problem
+# 1. Assigns 3 different values to 3 variables in one line
+# 2. Assigns one value to 3 variables in one line
+# 3. Create a list then unpack it
+# 4. Print all variables.
+
+name, age, course = "Brian Orea", 23, "BSCS"
+
+var1 = var2 = var3 = "Im a CS Student"
+
+OS = ["Windows", "MacOs", "Linux"]
+os1, os2, os3 = OS
+
+print(name)
+print(age)
+print(course)
+
+print(var1)
+print(var2)
+print(var3)
+
+print(os1)
+print(os2)
+print(os3)
