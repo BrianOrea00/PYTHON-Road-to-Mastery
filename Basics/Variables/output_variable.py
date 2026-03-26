545714c374bd38cef1c0f743875bcec80f03a15a
BrianOrea00 <deathscytheiv@gmail.com>
2026-03-26 22:06:11 +0800

    Output Variable

---
+++ Basics/Variables/output_variable.py
@@ -0,0 +1,33 @@
+# Output Variable
+# the print() function is often used to output Variable
+# example1
+x = "this is Output Variable"
+print(x)
+
+# we can print multiple variable using coma ( , )
+# example2
+a = "this is"
+b = "Output"
+c = "Variable"
+print(a, b, c)
+
+# we can also print multiple variable using plus sign ( + )
+# example3
+d = "I "
+f = "am "
+g = "Learning"
+print(d + f + g)
+
+# additional info as you can see in the variables d and f there is a space after the letter or word because if we dont put space in it it will combine in one sentence
+# if we didnt put space in it the output will be "IamLearning"
+# we can also add numbers 
+# example4
+h = 5
+i = 10
+print(h + i)
+
+# but we cant add different data type like string and integer
+# print(a + h) "TypeError: can only concatenate str (not "int") to str" this will show in the terminal
+# we can do this if we use coma instead of add sign
+print(a , h)
+
---
+++ Basics/Variables/output_variableAct.py
@@ -0,0 +1,16 @@
+# AI generated problem
+# 1. Create these variables: name, age, city.
+# 2. Print using coma ( , ): Hello I am "name" from "city" and I am "age" years old.
+# 3. Create these variables: a = 1111 and b = 9999
+# 4. Print using + operator: the num 3 problem.
+# 5. Debug this: print("Age: " + age)
+
+name = "Brian Orea"
+age = 23
+city = "Pandi"
+print("Hello I am", name ,"from", city, "and I am", age , "years old.")
+a = 1111
+b = 9999
+print(a + b)
+
+print("Age: ", age)
