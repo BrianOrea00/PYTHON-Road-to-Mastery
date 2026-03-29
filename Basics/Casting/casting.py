f31dff30c0ae0b370152c833fa11c222bd4ea633
BrianOrea00 <deathscytheiv@gmail.com>
2026-03-29 21:26:47 +0800

    Casting

---
+++ Basics/Casting/casting.py
@@ -0,0 +1,29 @@
+# Casting
+# there will be a time that we want to specify a type on a variable.
+# this can be done by casting.
+# python is a object oriented language and in such it uses classes to define data types.
+# casting in python is therefore done using constructor functions
+# what is constructor?
+# constructor is a special function that runs automatically when you create an object from a class.
+# we have int() construct an integer number from an integer literal, a float literal (by removing all decimals), or string literal (providing the string represents a whole number).
+# we have float() construct an float number frim an integer literal, a float literal or string literal (providing  the string represents a float or a integer).
+# we have str() construct an string from wide variety of data types, including string, integer literal and float literal.
+# example1(integer)
+x1 = int(1)
+x2 = int(2.9999999999)
+x3 = int("3")
+print(x1 ,x2 ,x3)
+
+#example2(float)
+y1 = float(1)
+y2 = float(2.2)
+y3 = float("3")
+y4 = float("4.1")
+print(y1 ,y2 ,y3, y4)
+
+#example3(string)
+z1 = str("qwer")
+z2 = str(2)
+z3 = str(3.0)
+print(z1 ,z2, z3)
+
---
+++ Basics/Casting/castingAct.py
@@ -0,0 +1,39 @@
+# AI generated problem
+# 1. Create this variables: a = 5, b = 2.8, c = "10" then convert them to a -> float, b -> int, c -> int then print the results.
+# 2. Create this variables: x = 25 and y = 10 convert to string then print the results.
+# 3. Debug this num1 = "15" and num2 = "5" print(num1 + num2)
+# 4.Create a program that: (Hint: input is always string)
+#   asks user input:
+#   age = input("Enter your age: ")
+#   Convert it properly and print:
+#   Next year: (age + 1)
+
+a = 5
+b = 2.8
+c = "10"
+conA = float(a)
+conB = int(b)
+conC = int(c)
+print(conA)
+print(conB)
+print(conC)
+
+x = 25
+y = 10
+conX = str(x)
+conY = str(y)
+print(conX)
+print(type(conX))
+print(conY)
+print(type(conY))
+
+num1 = "15"
+num2 = "5"
+conNum1 = int(num1)
+conNum2 = int(num2)
+print(conNum1 + conNum2)
+
+age = input("Enter your age: ")
+conAge = int(age)
+nextyear = conAge + 1
+print(nextyear)
